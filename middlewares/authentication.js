e92e1cfd08a1f597cc5688ef8a3d3b99bf6a0df0
Arvind <arvindbalwal01@gmail.com>
2024-05-15 09:09:26 +0530

    added return statements before calling next () , just good practice

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,16 +3,16 @@ const { verifyjwt } = require("../services/authentication")
 const fetchuser = (req,res,next)=>{
     // console.log(req.cookies)
     if(!req.cookies?.token){
-       next()
+      return next()
     }
     else{
         const user = verifyjwt(req.cookies.token)
         if(!user){
-            next()
+          return  next()
         }
         else{
             req.user = user
-            next()
+          return  next()
         }
     }
 }
